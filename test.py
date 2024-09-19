0820172ce317771ea40b4710d5b21eb3abefa04f
chae-sy <sendemailtocsy@gmail.com>
2024-09-19 23:17:29 +0900

    test

---
+++ test.py
@@ -0,0 +1,267 @@
+# Copyright (c) 2023 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+# Copyright (c) 2023 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+
+import torch.nn.functional as F
+from torch import nn
+
+
+
+class ConvBNReLU(nn.Module):
+    def __init__(
+        self,
+        in_plane,
+        out_plane,
+        idx,
+        kernel_size=3,
+        stride=1,
+        groups=1,
+        use_dilation=False,
+        activation=True,
+        swish=False,
+        BN=True,
+        ssn=False,
+    ):
+        super().__init__()
+
+        def get_padding(kernel_size, use_dilation):
+            rate = 1  # dilation rate
+            padding_len = (kernel_size - 1) // 2
+            if use_dilation and kernel_size > 1:
+                rate = int(2**self.idx)
+                padding_len = rate * padding_len
+            return padding_len, rate
+
+        self.idx = idx
+
+        # padding and dilation rate
+        if isinstance(kernel_size, (list, tuple)):
+            padding = []
+            rate = []
+            for k_size in kernel_size:
+                temp_padding, temp_rate = get_padding(k_size, use_dilation)
+                rate.append(temp_rate)
+                padding.append(temp_padding)
+        else:
+            padding, rate = get_padding(kernel_size, use_dilation)
+
+        # convbnrelu block
+        layers = []
+        layers.append(
+            nn.Conv2d(in_plane, out_plane, kernel_size, stride, padding, rate, groups, bias=False)
+        )
+        if ssn:
+            layers.append(SubSpectralNorm(out_plane, spec_groups=5))
+        elif BN:
+            layers.append(nn.BatchNorm2d(out_plane))
+        if swish:
+            layers.append(nn.SiLU(True))
+        elif activation:
+            layers.append(nn.ReLU6(True))
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class BCResBlock(nn.Module):
+    def __init__(self, in_plane, out_plane, idx, stride):
+        super().__init__()
+        self.transition_block = in_plane != out_plane
+        kernel_size = (3, 3)
+
+        # 2D part (f2)
+        layers = []
+        if self.transition_block:
+            layers.append(ConvBNReLU(in_plane, out_plane, idx, 1, 1))
+            in_plane = out_plane
+        layers.append(
+            ConvBNReLU(
+                in_plane,
+                out_plane,
+                idx,
+                (kernel_size[0], 1),
+                (stride[0], 1),
+                groups=in_plane,
+                ssn=True, # modified
+                activation=False,
+            )
+        )
+        self.f2 = nn.Sequential(*layers)
+        self.avg_gpool = nn.AdaptiveAvgPool2d((1, None))
+
+        # 1D part (f1)
+        self.f1 = nn.Sequential(
+            ConvBNReLU(
+                out_plane,
+                out_plane,
+                idx,
+                (1, kernel_size[1]),
+                (1, stride[1]),
+                groups=out_plane,
+                swish=False,
+                use_dilation=True,
+            ),
+            nn.Conv2d(out_plane, out_plane, 1, bias=False),
+            nn.Dropout2d(0.1),
+        )
+
+    def forward(self, x):
+        # 2D part
+        shortcut = x
+        x = self.f2(x)
+        aux_2d_res = x
+        x = self.avg_gpool(x)
+
+        # 1D part
+        x = self.f1(x)
+        x = x + aux_2d_res
+        if not self.transition_block:
+            x = x + shortcut
+        x = F.relu6(x, True)
+        return x
+
+
+def BCBlockStage(num_layers, last_channel, cur_channel, idx, use_stride):
+    stage = nn.ModuleList()
+    channels = [last_channel] + [cur_channel] * num_layers
+    for i in range(num_layers):
+        stride = (2, 1) if use_stride and i == 0 else (1, 1)
+        stage.append(BCResBlock(channels[i], channels[i + 1], idx, stride))
+    return stage
+
+
+class BCResNets(nn.Module):
+    def __init__(self, base_c, num_classes=12):
+        super().__init__()
+        self.num_classes = num_classes
+        self.n = [1, 1, 1]  # identical modules repeated n times
+        self.c = [
+            int(base_c * 2),
+            base_c,
+            int(base_c * 1.5),
+            base_c * 2,
+            #int(base_c * 2.5),
+            base_c * 4,
+        ]  # num channels
+        self.s = [1, 2]  # stage using stride
+        self._build_network()
+
+    def _build_network(self):
+        # Head: (Conv-BN-ReLU)
+        self.cnn_head = nn.Sequential(
+            nn.Conv2d(1, self.c[0], 5, (2, 1), 2, bias=False), #padding=2
+            nn.BatchNorm2d(self.c[0]),
+            nn.ReLU6(True),
+        )
+        # Body: BC-ResBlocks
+        self.BCBlocks = nn.ModuleList([])
+        for idx, n in enumerate(self.n):
+            use_stride = idx in self.s
+            self.BCBlocks.append(BCBlockStage(n, self.c[idx], self.c[idx + 1], idx, use_stride))
+
+        # Classifier
+        self.classifier = nn.Sequential(
+            #nn.Conv2d(
+            #    self.c[-2], self.c[-2], (5, 5), bias=False, groups=self.c[-2], padding=(0, 2)
+            #),
+            #nn.Conv2d(self.c[-2], self.c[-1], 1, bias=False),
+            #nn.BatchNorm2d(self.c[-1]),
+            #nn.ReLU6(True),
+            #nn.AdaptiveAvgPool2d((1, 1)),
+            #nn.Conv2d(self.c[-1], self.num_classes, 1),
+
+            #nn.Conv2d(self.c[-2], self.c[-1], kernel_size=1),
+            nn.BatchNorm2d(self.c[-2]),
+            nn.Conv2d(self.c[-2], self.num_classes, kernel_size=1),
+            nn.AdaptiveAvgPool2d(1),
+        )
+
+    def forward(self, x):
+        x = self.cnn_head(x)
+        for i, num_modules in enumerate(self.n):
+            for j in range(num_modules):
+                x = self.BCBlocks[i][j](x)
+        x = self.classifier(x)
+        x = x.view(-1, x.shape[1])
+        return x
+
+class SubSpectralNorm(nn.Module):
+    def __init__(self, num_features, spec_groups=16, affine="Sub", batch=True, dim=2):
+        super().__init__()
+        self.spec_groups = spec_groups
+        self.affine_all = False
+        affine_norm = False
+        if (
+            affine == "Sub"
+        ):  # affine transform for each sub group. use affine of torch implementation
+            affine_norm = True
+        elif affine == "All":
+            self.affine_all = True
+            self.weight = nn.Parameter(torch.ones((1, num_features, 1, 1)))
+            self.bias = nn.Parameter(torch.zeros((1, num_features, 1, 1)))
+        if batch:
+            self.ssnorm = nn.BatchNorm2d(num_features * spec_groups, affine=affine_norm)
+        else:
+            self.ssnorm = nn.InstanceNorm2d(num_features * spec_groups, affine=affine_norm)
+        self.sub_dim = dim
+
+    def forward(self, x):  # when dim h is frequency dimension
+        if self.sub_dim in (3, -1):
+            x = x.transpose(2, 3)
+            x = x.contiguous()
+        b, c, h, w = x.size()
+        assert h % self.spec_groups == 0
+        x = x.view(b, c * self.spec_groups, h // self.spec_groups, w)
+        x = self.ssnorm(x)
+        x = x.view(b, c, h, w)
+        if self.affine_all:
+            x = x * self.weight + self.bias
+        if self.sub_dim in (3, -1):
+            x = x.transpose(2, 3)
+            x = x.contiguous()
+        return x
+
+
+# model=BCResNets(base_c=1)
+# model.to("cuda")
+
+import torch
+
+class BCResNetsWithDebug(BCResNets):
+    def forward(self, x):
+        print(f"Input shape: {x.shape}")
+        
+        # Head: (Conv-BN-ReLU)
+        x = self.cnn_head(x)
+        print(f"After cnn_head: {x.shape}")
+        
+        # Body: BC-ResBlocks
+        for i, num_modules in enumerate(self.n):
+            for j in range(num_modules):
+                x = self.BCBlocks[i][j](x)
+                print(f"After BCBlock[{i}][{j}]: {x.shape}")
+        
+        # Classifier
+        x = self.classifier(x)
+        print(f"After classifier: {x.shape}")
+        
+        x = x.view(-1, x.shape[1])
+        print(f"Final output shape (after view): {x.shape}")
+        
+
+        return x
+
+model = BCResNetsWithDebug(base_c=8, num_classes=12)
+    
+# (batch_size, channels, height, width) 크기의 랜덤 텐서 생성
+input_tensor = torch.randn(1, 1, 40, 61)  # 1개의 샘플, 1채널, 높이 128, 너비 64
+    
+# 모델에 입력
+output = model(input_tensor)
